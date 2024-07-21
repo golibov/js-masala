4ba7981a6513e83f143d78f0a24192dd70707177
golibov <golibovshg7@gmail.com>
2024-07-21 11:26:43 +0500

    the end 50%

---
+++ js/app.js
@@ -0,0 +1,115 @@
+"use strict";
+
+// let speed = +prompt("Tezlikni kiriting");
+
+// if (speed <= 70) {
+//  alert('Hammasi yaxshi')
+// }
+// else{
+//     let point = Math.floor((speed - 70) / 5);
+//     if(point>= 120){
+//         alert("Prava olindi chunki sizda " + point + "ta point bor")
+//     }
+//     else{
+//         alert(point);
+//     }
+// }
+
+
+//============> 1-masala <============
+// const optional = [20,50,100];
+// let sum = 0;
+
+// for (let i = 0; i < optional.length; i++) {
+//   sum += optional[i];
+// }
+
+
+// console.log(`Yig'indisi: ${sum}`);
+
+
+//==============> 2-masala <==============
+
+// let enterDollar = +prompt("Dollarni kiriting");
+// let som = 12500;
+
+// if(enterDollar >= 0){
+//     alert(`${enterDollar * som} so'm`);
+// }
+
+
+//============> 3-masala <==============
+
+// let enterNumber = +prompt('Son kiriting');
+// let result = enterNumber **2;
+
+// if(result%2==0){
+//     alert(`${result} - juft son`)
+// }
+// else if(result**2%2==1){
+//     alert(`${result} - toq son`)
+// }
+// else{
+//     alert('Bu dastur faqat kvadratini hisoblab to yoki jufligini aniqlab beradi')
+// }
+
+//============> 4-masala <==============
+
+// let userInput = prompt("Sonni kiriting");
+// let number = parseInt(userInput);
+
+// let sum = 0;
+
+// for (let i = 1; i <= number; i++) {
+//   if (i % 3 === 0) {
+
+//     sum += i;
+//   }
+// }
+
+// console.log(`Natija: ${sum}`);
+
+
+//============> 5-masala <==============
+
+// Tushunmadim
+
+
+
+
+
+// ---------------------> 2- rasm <-------------------
+
+//=============> 1-masala  <==============
+
+// let arr = [1, 55, 63, 12];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr.length - 3 % 2 == 0) {
+//         alert(`bu son juft son`)
+//     } else {
+//         alert(`bu son toq son`)
+//     }
+// }
+
+
+//=============> 2-masala  <==============
+
+// for (let i=1; i<=50; i++){
+//     console.log(i);
+// }
+
+//================> 3-masala <=============
+
+// let enterNumber = +prompt('Son kiriting');
+
+// let sum = 0 
+
+// for (let i = 1; i <= enterNumber; i++) {
+//   sum+=i
+    
+// }
+
+// console.log(`1 dan ${enterNumber} gacha bo'lgan sonlar yig'indisi: ${sum}`);
+
+//================> 4-masala <=============
